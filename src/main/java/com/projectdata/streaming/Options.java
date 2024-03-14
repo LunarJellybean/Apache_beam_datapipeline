cc91b6b6cbc06462bc699defb9d51cd604fd2be6
MayankSinku <63499578+LunarJellybean@users.noreply.github.com>
2024-03-14 19:07:56 +0530

    Updating pom.xml, updating Options.java and StreamingPipeline.java files

---
+++ src/main/java/com/projectdata/streaming/Options.java
@@ -0,0 +1,17 @@
+package com.projectdata.streaming;
+
+import org.apache.beam.sdk.options.Default;
+import org.apache.beam.sdk.options.Description;
+import org.apache.beam.sdk.options.PipelineOptions;
+
+public interface Options extends PipelineOptions {
+    @Description("Path of the input file")
+    @Default.String("gs://apache-beam-samples/shakespeare/kinglear.txt")
+    String getInputFile();
+    void setInputFile(String value);
+
+    @Description("Path of the output file")
+    @Default.String("gs://bucket-wiseen/dataflow/output-2.txt")
+    String getOutputFile();
+    void setOutputFile(String value);
+}
