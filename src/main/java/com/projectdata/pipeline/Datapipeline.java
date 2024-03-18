f6aaae5b5318ab397ef1c0ee8d4e349e34d4a013
MayankSinku <63499578+LunarJellybean@users.noreply.github.com>
2024-03-18 19:28:27 +0530

    Name update and addition of Transformation file

--- src/main/java/com/projectdata/pipeline/Datapipeline.java
+++ src/main/java/com/projectdata/pipeline/Datapipeline.java
@@ -1,20 +1,25 @@
-package com.projectdata.streaming;
+package com.projectdata.pipeline;
 
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.PipelineResult;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PDone;
 
-public class StreamingPipeline {
+public class Datapipeline {
     public static void main(String[] args) {
         Options options = PipelineOptionsFactory.fromArgs(args).withValidation().as(Options.class);
         Pipeline p = Pipeline.create(options);
 
         //Reading from public dataset containing King Lear text
-        PCollection<String> input = p.apply(("Read"), TextIO.read().from("gs://apache-beam-samples/shakespeare/kinglear.txt"));
-        PDone done = input.apply(("Write"), TextIO.write().to("gs://projectdata-202403-bucket/dataflow/output.txt"));
+        PCollection<String> extractedData = p.apply(("Read"), TextIO.read().from("gs://apache-beam-samples/shakespeare/kinglear.txt"));
+                            //Reading from a public bucket
+
+        PCollection<String> transformedData = extractedData.apply("Transform", ParDo.of(new Transformation()));
+
+        PDone done = transformedData.apply(("Write"), TextIO.write().to("gs://projectdata-202403-bucket/dataflow/output.txt"));
 
         PipelineResult result = p.run();
         try {
---
+++ src/main/java/com/projectdata/pipeline/Transformation.java
@@ -0,0 +1,16 @@
+package com.projectdata.pipeline;
+
+import org.apache.beam.sdk.transforms.DoFn;
+
+public class Transformation extends DoFn<String, String> {
+
+    private static final long serialVersionUID = -5259879110421558885L;
+    @ProcessElement
+    public void processElement(ProcessContext c) {
+
+        String line = c.element();
+        line = line.toUpperCase();
+
+        c.output(line);
+    }
+}
