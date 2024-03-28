bff5c4adb5ce4828d03d0aa7f436482baf92dbbb
MayankSinku <63499578+LunarJellybean@users.noreply.github.com>
2024-03-28 13:50:05 +0530

    Added the created test data file and updated Datapipeline code to handle exceptions (#5)
    * Handling exceptions
    
    * Adding the created test data file

--- src/main/java/com/projectdata/pipeline/Datapipeline.java
+++ src/main/java/com/projectdata/pipeline/Datapipeline.java
@@ -2,25 +2,51 @@ package com.projectdata.pipeline;
 
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.PipelineResult;
+import org.apache.beam.sdk.coders.StringUtf8Coder;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.PCollectionTuple;
 import org.apache.beam.sdk.values.PDone;
+import org.apache.beam.sdk.values.TupleTagList;
 
 public class Datapipeline {
     public static void main(String[] args) {
         Options options = PipelineOptionsFactory.fromArgs(args).withValidation().as(Options.class);
         Pipeline p = Pipeline.create(options);
 
-        //Reading from public dataset containing King Lear text
-        PCollection<String> extractedData = p.apply(("Read"), TextIO.read().from("gs://apache-beam-samples/shakespeare/kinglear.txt"));
-                            //Reading from a public bucket
 
-        PCollection<String> transformedData = extractedData.apply("Transform", ParDo.of(new Transformation()));
+//        //Reading from public dataset containing King Lear text
+//        PCollection<String> extractedData = p.apply(("Read"), TextIO.read().from("gs://apache-beam-samples/shakespeare/kinglear.txt"));
+//                            //Reading from a public bucket
+
+        //We are dropping a file to the bucket
+        PCollection<String> extractedData = p.apply(("Extract"),
+                TextIO.read().from("gs://projectdata-202403-bucket/dataflow/zillow.csv"));
+
+
+
+//        PCollection<String> transformedData = extractedData.apply("Transform", ParDo.of(new Transformation()));
+
+        //execute transform to calculate the price per sqft for hourses
+        PCollectionTuple transformedTuple = extractedData.apply("Transform", ParDo.of(new Transformation())
+                .withOutputTags(Transformation.VALID_DATA_TAG, TupleTagList.of(Transformation.FAILURE_DATA_TAG)));
+
+        //save data for known sized house
+        transformedTuple.get(Transformation.VALID_DATA_TAG)
+                .setCoder(StringUtf8Coder.of())
+                .apply("Save Result", TextIO.write().to("gs://projectdata-202403-bucket/dataflow/zillow-result.txt"));
+
+        //handle exception for unknown sized house
+        transformedTuple.get(Transformation.FAILURE_DATA_TAG)
+                .setCoder(StringUtf8Coder.of())
+                .apply("Unknown Sqft data", TextIO.write().to("gs://projectdata-202403-bucket/dataflow/zillow-unknown.txt"));
+
+
 
 //        PDone done = transformedData.apply(("Write"), TextIO.write().to("gs://projectdata-202403-bucket/dataflow/output.txt"));
-        PDone done = transformedData.apply(("Write"), TextIO.write().to("gs://projectdata-202403-bucket/dataflow/transformed-output.txt"));
+//        PDone done = transformedData.apply(("Write"), TextIO.write().to("gs://projectdata-202403-bucket/dataflow/transformed-output.txt"));
         //update the file name else it will override the existing file
 
         PipelineResult result = p.run();
